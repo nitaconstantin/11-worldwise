33a699207e6701c188618b4c2ef7b493ff600532
nitaconstantin <104198432+nitaconstantin@users.noreply.github.com>
2025-12-22 20:31:14 +0200

    213 - Implementing the City List

---
+++ src/components/CityList.jsx
@@ -0,0 +1,17 @@
+import styles from "./CityList.module.css";
+import Spinner from "../components/Spinner";
+import CityItem from "./CityItem";
+import Message from "../components/Message";
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length) return <Message message="You have to add first a city" />;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
